9de2e3fc30c1df84eb1d3c687b74b76313e523c3
Łukasz Adach <lukasz.adach7@gmail.com>
2018-04-17 23:40:07 +0200

    added models

---
+++ Targets/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Targets.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string NickName { get; set; }
+        public string Password { get; set; }
+        public IEnumerable<Target> Targets { get; set; }
+
+    }
+}
