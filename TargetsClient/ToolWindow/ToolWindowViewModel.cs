2290e6062061ac72ddf4fc7b6cdf8909e088be7b
Łukasz Adach <lukasz.adach7@gmail.com>
2018-05-14 23:32:54 +0200

    simple validation

--- TargetsClient/ToolWindow/ToolWindowViewModel.cs
+++ TargetsClient/ToolWindow/ToolWindowViewModel.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Input;
 
 namespace TargetsClient.ToolWindow
 {
@@ -19,5 +20,50 @@ namespace TargetsClient.ToolWindow
         }
 
 
+        private string title="";
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value;
+                RaisePropertyChangedEvent("Title");
+            }
+
+        }
+
+
+
+
+        private string description="";
+
+        public string Description       
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                RaisePropertyChangedEvent("Description");
+            }
+
+        }
+
+
+
+
+        public ICommand CmdAdd { get { return new RelayCommand(x => CanAdd(x), x => Add(x)); } }
+
+        private bool CanAdd(object x)
+        {
+            if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Description))
+                return true;
+
+
+            return false;
+        }
+
+        private void Add(object x)
+        {
+
+        }
     }
 }
